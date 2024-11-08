fdf622b2d2944ceebbbecbc6e1c17699849ae68b
phennessey <1308575+phennessey@users.noreply.github.com>
2024-11-08 12:24:41 -0800

    Update ase2json.js
    Updated to handle LAB values, also changed to Buffer.from() (Buffer() is deprecated due to security/stability issues)

--- ase2json.js
+++ ase2json.js
@@ -1,10 +1,9 @@
 /*
  * Adobe Swatch Exchange (ASE) to JSON Converter
  * Copyright © Marco Lehmann <m99coder@gmail.com> 2014
- *
+ * Updated to handle LAB values by phennessey, 2024
  * File Format: http://www.selapa.net/swatches/colors/fileformats.php#adobe_ase
  */
-
 
 // require modules
 var util = require('util'),
@@ -12,21 +11,19 @@ var util = require('util'),
   fs = require('fs'),
   binary = require('binary');
 
-
 // read arguments
 var file = (process.argv[2]) ? process.argv[2] : null;
 if (file == null) {
   return console.error('ERROR: no ASE file given\nUSAGE: node ase2json.js file.ase');
 }
 
-
 // signature
 const SIGNATURE = 'ASEF';
 
 // block types
-const BT_GROUP_START = new Buffer([0xC0, 0x01]);
-const BT_GROUP_END = new Buffer([0xc0, 0x02]);
-const BT_COLOR_ENTRY = new Buffer([0x00, 0x01]);
+const BT_GROUP_START = new Buffer.from([0xC0, 0x01]);
+const BT_GROUP_END = new Buffer.from([0xc0, 0x02]);
+const BT_COLOR_ENTRY = new Buffer.from([0x00, 0x01]);
 
 // color modes
 const CM_CMYK = 'CMYK';
@@ -38,7 +35,6 @@ const CM_GRAY = 'Gray';
 const CT_GLOBAL = 0;
 const CT_SPOT = 1;
 const CT_NORMAL = 2;
-
 
 // swap bytes from big-endian to little-endian
 var swapBytes = function(buffer) {
@@ -54,10 +50,8 @@ var swapBytes = function(buffer) {
   return buffer; 
 };
 
-
 // read file
 fs.readFile(file, function(err, data) {
-
   // error reading file
   if (err) {
     return console.error(err.message);
@@ -72,7 +66,6 @@ fs.readFile(file, function(err, data) {
     return console.error('ERROR: no Adobe Swatch Exchange file');
   }
 
-
   // parse data
   binary.parse(data)
     .buffer('signature', 4)                                     // ASE signature: 4 * char
@@ -81,7 +74,6 @@ fs.readFile(file, function(err, data) {
 
     // Blocks
     .loop(function(endBlocks, vars) {
-
       // end of file reached
       if (this.eof()) {
         endBlocks();
@@ -93,16 +85,12 @@ fs.readFile(file, function(err, data) {
 
         // Switch Block Types
         .tap(function(vars) {
-
           // Group Start
           if (vars.blockType.toString() == BT_GROUP_START.toString()) {
-
             this.word16bu('groupNameLength')                    // Group Name Length: 1 * int16
               .buffer('groupName', vars.groupNameLength * 2);   // Group Name: groupNameLength * int16 (null terminated)
-
             vars.currentGroup = swapBytes(vars.groupName).toString('utf16le');
             vars.currentGroup = vars.currentGroup.substring(0, vars.currentGroup.length - 1);
-
           }
 
           // Group End
@@ -110,14 +98,12 @@ fs.readFile(file, function(err, data) {
 
           // Color Entry
           if (vars.blockType.toString() == BT_COLOR_ENTRY.toString()) {
-
             this.word16bu('colorNameLength')                    // Color Name Length: 1 * int16
               .buffer('colorName', vars.colorNameLength * 2)    // Color Name: colorNameLength * int16 (null terminated)
               .buffer('colorModel', 4);                         // Color Model: 4 * char
 
             // CMYK
             if (vars.colorModel.toString().trim() == CM_CMYK) {
-              
               this.buffer('cyan', 4)                            // Color Definition: 4 * int32
                 .buffer('magenta', 4)
                 .buffer('yellow', 4)
@@ -128,126 +114,89 @@ fs.readFile(file, function(err, data) {
               vars.yellowFloat = vars.yellow.readFloatBE(0);
               vars.keyFloat = vars.key.readFloatBE(0);
 
-              vars.redFromCMYK = Math.round(((1 - vars.cyanFloat) * (1 - vars.keyFloat)) * 255);
-              vars.greenFromCMYK = Math.round(((1 - vars.magentaFloat) * (1 - vars.keyFloat)) * 255);
-              vars.blueFromCMYK = Math.round(((1 - vars.yellowFloat) * (1 - vars.keyFloat)) * 255);
-
+              var color = {
+                'model': CM_CMYK,
+                'type': vars.colorType,
+                'cmyk': [
+                  vars.cyanFloat,
+                  vars.magentaFloat,
+                  vars.yellowFloat,
+                  vars.keyFloat
+                ],
+                'rgb': [
+                  Math.round(((1 - vars.cyanFloat) * (1 - vars.keyFloat)) * 255),
+                  Math.round(((1 - vars.magentaFloat) * (1 - vars.keyFloat)) * 255),
+                  Math.round(((1 - vars.yellowFloat) * (1 - vars.keyFloat)) * 255)
+                ]
+              };
             }
 
             // RGB
             if (vars.colorModel.toString().trim() == CM_RGB) {
-
               this.buffer('red', 4)                             // Color Definition: 3 * int32
                 .buffer('green', 4)
                 .buffer('blue', 4);
 
-              vars.red = Math.round(vars.red.readFloatBE(0) * 255);
-              vars.green = Math.round(vars.green.readFloatBE(0) * 255);
-              vars.blue = Math.round(vars.blue.readFloatBE(0) * 255);
+              var color = {
+                'model': CM_RGB,
+                'type': vars.colorType,
+                'rgb': [
+                  Math.round(vars.red.readFloatBE(0) * 255),
+                  Math.round(vars.green.readFloatBE(0) * 255),
+                  Math.round(vars.blue.readFloatBE(0) * 255)
+                ]
+              };
+            }
 
+            // LAB
+            if (vars.colorModel.toString().trim() == CM_LAB) {
+              this.buffer('l', 4)                               // L* value: 1 * float32
+                  .buffer('a', 4)                               // a* value: 1 * float32
+                  .buffer('b', 4);                              // b* value: 1 * float32
+
+              var color = {
+                'lab': [
+                  Math.round(vars.l.readFloatBE(0)*255),
+                  vars.a.readFloatBE(0)+128,
+                  vars.b.readFloatBE(0)+128
+                ]
+              };
             }
 
             this.word16bu('colorType');                         // Color Type: 1 * int16
+            var colorName = swapBytes(vars.colorName).toString('utf16le');
+            colorName = colorName.substring(0, colorName.length - 1);
 
-            var colorType = 'Unknown';
-            if (vars.colorType == CT_GLOBAL) {
-              colorType = 'Global';
-            }
-            if (vars.colorType == CT_SPOT) {
-              colorType = 'Spot';
-            }
-            if (vars.colorType == CT_NORMAL) {
-              colorType = 'Normal';
-            }
-
-            // initialize groups if undefined
             if (typeof vars.groups == 'undefined') {
               vars.groups = {};
             }
+            var groupName = (typeof vars.currentGroup != 'undefined') ? vars.currentGroup : null;
 
-            var colorName = swapBytes(vars.colorName).toString('utf16le');
-            colorName = colorName.substring(0, colorName.length - 1);
-            
-            groupName = (typeof vars.currentGroup != 'undefined') ? vars.currentGroup : null;
-
-            var color = {
-              'model': vars.colorModel.toString().trim(),
-              'type': vars.colorType
-            };
-
-            if (vars.colorModel.toString().trim() == CM_CMYK) {
-              color.cmyk = [
-                vars.cyanFloat,
-                vars.magentaFloat,
-                vars.yellowFloat,
-                vars.keyFloat
-              ];
-              color.rgb = [
-                vars.redFromCMYK,
-                vars.greenFromCMYK,
-                vars.blueFromCMYK
-              ];
-              color.code =
-                ('00' + vars.redFromCMYK.toString(16)).slice(-2) +
-                ('00' + vars.greenFromCMYK.toString(16)).slice(-2) +
-                ('00' + vars.blueFromCMYK.toString(16)).slice(-2);
-              console.log(colorName, color);
-            }
-
-            if (vars.colorModel.toString().trim() == CM_RGB) {
-              color.rgb = [
-                vars.red,
-                vars.green,
-                vars.blue
-              ];                
-              color.code =
-                ('00' + vars.red.toString(16)).slice(-2) +
-                ('00' + vars.green.toString(16)).slice(-2) +
-                ('00' + vars.blue.toString(16)).slice(-2);
-            }
-            
             // insert into group if defined, otherwise root element
             if (groupName != null) {
-
               if (typeof vars.groups[groupName] == 'undefined') {
                 vars.groups[groupName] = {};
               }
-
               vars.groups[groupName][colorName] = color;
-
             } else {
-
               vars.groups[colorName] = color;
-
             }
-
           }
-
         });
-
     })
-
-    // Output
     .tap(function(vars) {
-      
       console.log('Signature: ' + vars.signature.toString());
       console.log('Version: ' + vars.version.readUInt16BE(0) + '.' + vars.version.readUInt16BE(2));
       console.log('Number of Blocks: ' + vars.numOfBlocks.readUInt32BE(0));
 
       var jsonFileName = path.dirname(file) + '/' + path.basename(file, '.ase') + '.json';
-      
+
       // write json file
       fs.writeFile(jsonFileName, JSON.stringify(vars.groups, null, 4), function(err) {
-        
-        // error writing file
         if (err) {
           return console.error(err.message);
         }
-
         console.log('Successfully written to ' + jsonFileName);
-  
       });
-      
     });
-
 });
